c97c9d026051be0e5ad17327b9ebdadb8564858c
shreyaan <Shreyaans41@gmail.com>
2023-01-21 20:56:50 +0530

    .

--- routes/songs.js
+++ routes/songs.js
@@ -5,5 +5,6 @@ const songController = require('../controllers/songController');
 router.get('/', songController.getAllSongs);
 router.get('/search',songController.searchSongs);
 router.get('/:id', songController.getSong);
+router.get('/justsongs', songController.JustSongs);
 
 module.exports = router;

0282a8d79012eb0a23f8a1666b047830564d3b95
shreyaan <Shreyaans41@gmail.com>
2023-01-21 20:54:05 +0530

    exports.JustSongs

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,47 +1,61 @@
 const axios = require("axios");
 const Song = require("../models/Song");
-const { v4: uuidv4 } = require('uuid');
-
-
+const { v4: uuidv4 } = require("uuid");
 
 exports.getAllSongs = async (req, res) => {
   try {
-      console.log("Refreshing songs...");
-      const response = await axios.get(
-          `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${process.env.LAST_FM}&format=json`
-      );
-      const songs = response.data.tracks.track;
-      console.log(songs);
+    console.log("Refreshing songs...");
+    const response = await axios.get(
+      `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${process.env.LAST_FM}&format=json`
+    );
+    const songs = response.data.tracks.track;
+    console.log(songs);
 
-     
-      const songPromises = songs.map(async (song) => {
-          const existingSong = await Song.findOne({song_id:song.mbid });
-          if (!existingSong) {
-              // Save song
-              if(!song.mbid){
-                  song.mbid = uuidv4();
-              }
+    const songPromises = songs.map(async (song) => {
+      const existingSong = await Song.findOne({ song_id: song.mbid });
+      if (!existingSong) {
+        // Save song
+        if (!song.mbid) {
+          song.mbid = uuidv4();
+        }
 
-            
-              const newSong = new Song({
-                  song_id:song.mbid,
-                  name:song.name,
-                  artist:song.artist?.name,
-                  album:song.album?.name,
-                  duration:song?.duration,
-                  preview_url:song?.url,
-              });
-              await newSong.save();
-          }
-      });
-      await Promise.all(songPromises);
+        const newSong = new Song({
+          song_id: song.mbid,
+          name: song.name,
+          artist: song.artist?.name,
+          album: song.album?.name,
+          duration: song?.duration,
+          preview_url: song?.url,
+        });
+        await newSong.save();
+      }
+    });
+    await Promise.all(songPromises);
 
-      // Retrieve all songs from the database
-      const dbSongs = await Song.find({});
+    // Retrieve all songs from the database
+    const dbSongs = await Song.find({});
 
-      return res.json({ message: "songs retrieved successfully", songs: dbSongs });
+    return res.json({
+      message: "songs retrieved successfully",
+      songs: dbSongs,
+    });
   } catch (error) {
-    if(!res.headersSent) return res.status(500).json({ message: error.message });
+    if (!res.headersSent)
+      return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.JustSongs = async (req, res) => {
+  try {
+    // Retrieve all songs from the database
+    const dbSongs = await Song.find({});
+    return res.json({
+      message: "songs retrieved successfully",
+      songs: dbSongs,
+    });
+  } catch (error) {
+    if (!res.headersSent)
+      return res.status(500).json({ message: error.message });
   }
 };
 
@@ -62,4 +76,3 @@ exports.searchSongs = (req, res) => {
     return res.json(songs);
   });
 };
-

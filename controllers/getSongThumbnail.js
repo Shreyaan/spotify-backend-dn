455fc061e7ad05c8d5e58e8542447291e55a36ef
shreyaan <Shreyaans41@gmail.com>
2023-01-22 19:54:47 +0530

    .

--- controllers/getSongThumbnail.js
+++ controllers/getSongThumbnail.js
@@ -11,24 +11,26 @@ exports.getSongThumbnails = async (req, res) => {
     let songObj;
     let returned = false;
 
-   await Song.findById(songId).then((song) => {
-      if (!song){
+    await Song.findById(songId).then((song) => {
+      if (!song) {
         returned = true;
-       return axiosImage(
+        return axiosImage(
           "https://www.digitalmesh.com/blog/wp-content/uploads/2020/05/404-error.jpg",
           res
-        );}
+        );
+      }
 
       if (song.image_url) {
         returned = true;
-        if(!res.headersSent) return axiosImage(song.image_url, res);
+        if (!res.headersSent) return axiosImage(song.image_url, res);
       }
       if (!song?.preview_url) {
         returned = true;
         return axiosImage(
           "https://www.digitalmesh.com/blog/wp-content/uploads/2020/05/404-error.jpg",
           res
-        );}
+        );
+      }
 
       songObj = song;
     });
@@ -38,13 +40,13 @@ exports.getSongThumbnails = async (req, res) => {
     if (!browser) {
       browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox','--disable-setuid-sandbox']
-      })
-      
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
     }
     const thumbnail = await getThumbnail(songObj.preview_url, browser);
     if (!thumbnail) {
-        if(!res.headersSent)  return res.status(404).json({ message: "Thumbnail not found" });
+      if (!res.headersSent)
+        return res.status(404).json({ message: "Thumbnail not found" });
     }
 
     if (songId) {
@@ -61,14 +63,15 @@ exports.getSongThumbnails = async (req, res) => {
           }
         }
       );
-    } 
+    }
 
     await axiosImage(thumbnail, res);
   } catch (err) {
-    if(!res.headersSent) axiosImage(
+    if (!res.headersSent)
+      axiosImage(
         "https://www.digitalmesh.com/blog/wp-content/uploads/2020/05/404-error.jpg",
         res
-      )
+      );
     console.log(err);
   }
 };
@@ -82,13 +85,10 @@ async function axiosImage(thumbnail, res) {
 async function getThumbnail(url, browserInstance) {
   try {
     const page = await browserInstance.newPage();
-    await page.goto(url, { waitUntil: "networkidle0" });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
     const thumbnail = await page.evaluate(() => {
       const img = document.querySelector(".js-video-preview-playlink img");
-      if (img) {
-        return img.getAttribute("src");
-      }
-      return null;
+      return img ? img.getAttribute("src") : null;
     });
     await page.close();
     return thumbnail;
